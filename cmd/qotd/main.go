3e63a3598c14c3a4a1c00bd6bec21ed504c34248
Jelani Jackson <jeljack86@gmail.com>
2020-05-22 16:41:16 -0500

    Updated main to use built functionality

--- cmd/qotd/main.go
+++ cmd/qotd/main.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/morpheusnephew/qotd/internal/paperquotes"
 )
 
 func main() {
 
-	response := &paperquotes.QuoteOfTheDayResponse{
-		Author:   "Confucius",
-		Quote:    "Those who say they can and those who say they can't are both usually right.",
-		Language: "en",
-		Likes:    10,
-		Tags:     []string{"Inspirational"},
-	}
+	response, errorResponse := paperquotes.GetQuoteOfTheDay()
 
-	fmt.Println(response)
+	if errorResponse != nil {
+		log.Fatalln(errorResponse)
+	} else {
+		fmt.Println(response)
+	}
 }
