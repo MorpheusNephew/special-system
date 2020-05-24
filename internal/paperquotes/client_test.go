57a739ea7f87f0a539109c90b903245ee7705bb9
Jelani Jackson <jeljack86@gmail.com>
2020-05-23 19:07:32 -0500

    Added test for getResponse

--- internal/paperquotes/client_test.go
+++ internal/paperquotes/client_test.go
@@ -1,11 +1,25 @@
 package paperquotes
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/morpheusnephew/qotd/internal/testutils"
 )
+
+type MockClient struct {
+}
+
+func (c *MockClient) Do(req *http.Request) (*http.Response, error) {
+	body := ioutil.NopCloser(strings.NewReader(`{ "data" : "hello world!!!"}`))
+	return &http.Response{
+		StatusCode: 200,
+		Body:       body,
+	}, nil
+}
 
 func Test_getRequest(t *testing.T) {
 	expectedMethod := http.MethodGet
@@ -15,4 +29,20 @@ func Test_getRequest(t *testing.T) {
 
 	testutils.IfStringsNotEqual(t, r.Method, expectedMethod)
 	testutils.IfStringsNotEqual(t, r.URL.String(), expectedURL)
+}
+
+func Test_getResponse(t *testing.T) {
+	client = &MockClient{}
+
+	body, err := getResponse(&http.Request{})
+
+	if err != nil {
+		t.Errorf("Expected %v, but received %v", nil, err)
+	}
+
+	var res map[string]string
+
+	json.Unmarshal(body, &res)
+
+	testutils.IfStringsNotEqual(t, res["data"], "hello world!!!")
 }
