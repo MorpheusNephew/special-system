24af729ef39a8714c69818bc4f493d01b347004f
Jelani Jackson <jeljack86@gmail.com>
2020-05-24 12:13:51 -0500

    Added other variables

--- internal/variables/types.go
+++ internal/variables/types.go
@@ -10,13 +10,21 @@ var (
 	// PaperQuotesToken is the token used to get the quote of the day
 	PaperQuotesToken string
 
+	// RedisEndpoint is the endpoint for interacting with Redis
+	RedisEndpoint string
+
 	// RedisKey is the key used for getting/setting quote of the day in Redis
 	RedisKey string
+
+	// RedisPort is the port used with interacting with Redis
+	RedisPort string
 )
 
 func init() {
 	godotenv.Load()
 
 	PaperQuotesToken = os.Getenv("PAPER_QUOTES_TOKEN")
+	RedisEndpoint = os.Getenv("REDIS_ENDPOINT")
 	RedisKey = os.Getenv("REDIS_KEY")
+	RedisPort = os.Getenv("REDIS_PORT")
 }
