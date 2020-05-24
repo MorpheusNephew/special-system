7d9cb0fbd0c59beabd7cd7d497f2e424b7f13b65
Jelani Jackson <jeljack86@gmail.com>
2020-05-23 19:26:41 -0500

    Added additional test for testing getRequest ith an unsuccessful error code

--- internal/paperquotes/client_test.go
+++ internal/paperquotes/client_test.go
@@ -10,13 +10,25 @@ import (
 	"github.com/morpheusnephew/qotd/internal/testutils"
 )
 
-type MockClient struct {
+type SuccessfulMockClient struct {
 }
 
-func (c *MockClient) Do(req *http.Request) (*http.Response, error) {
+func (c *SuccessfulMockClient) Do(req *http.Request) (*http.Response, error) {
 	body := ioutil.NopCloser(strings.NewReader(`{ "data" : "hello world!!!"}`))
 	return &http.Response{
 		StatusCode: 200,
+		Body:       body,
+	}, nil
+}
+
+type UnsuccessfulMockClient struct {
+}
+
+func (c *UnsuccessfulMockClient) Do(req *http.Request) (*http.Response, error) {
+	body := ioutil.NopCloser(strings.NewReader(`{ "data" : "hello world!!!"}`))
+	return &http.Response{
+		StatusCode: 400,
+		Status:     "400 Bad Request",
 		Body:       body,
 	}, nil
 }
@@ -31,18 +43,23 @@ func Test_getRequest(t *testing.T) {
 	testutils.IfStringsNotEqual(t, r.URL.String(), expectedURL)
 }
 
-func Test_getResponse(t *testing.T) {
-	client = &MockClient{}
+func Test_getResponse_Successfully(t *testing.T) {
+	client = &SuccessfulMockClient{}
 
-	body, err := getResponse(&http.Request{})
-
-	if err != nil {
-		t.Errorf("Expected %v, but received %v", nil, err)
-	}
+	body, _ := getResponse(&http.Request{})
 
 	var res map[string]string
 
 	json.Unmarshal(body, &res)
 
 	testutils.IfStringsNotEqual(t, res["data"], "hello world!!!")
+}
+
+func Test_getResponse_Unsuccessfully(t *testing.T) {
+	client = &UnsuccessfulMockClient{}
+
+	_, err := getResponse(&http.Request{})
+
+	testutils.IfIntsNotEqual(t, err.Code, 400)
+	testutils.IfStringsNotEqual(t, err.Message, "Bad Request")
 }
