584f0ee173bb329f38e38bda6c10a10845055493
Jelani Jackson <jeljack86@gmail.com>
2020-05-25 14:06:03 -0500

    Added variable for current environment

--- internal/variables/types.go
+++ internal/variables/types.go
@@ -7,6 +7,9 @@ import (
 )
 
 var (
+	// Environment is the environment that is currently running this applications
+	Environment string
+
 	// PaperQuotesToken is the token used to get the quote of the day
 	PaperQuotesToken string
 
@@ -23,6 +26,7 @@ var (
 func init() {
 	godotenv.Load()
 
+	Environment = os.Getenv("ENV")
 	PaperQuotesToken = os.Getenv("PAPER_QUOTES_TOKEN")
 	RedisEndpoint = os.Getenv("REDIS_ENDPOINT")
 	RedisKeyPrefix = os.Getenv("REDIS_KEY_PREFIX")
