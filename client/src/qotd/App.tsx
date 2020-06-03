cb30dbb77c08a1684338a8a2a54b019961e20af3
Jelani Jackson <jeljack86@gmail.com>
2020-06-03 08:49:26 -0500

    Created Qotd App

---
+++ client/src/qotd/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Header, Footer, Body } from "./components";
+
+const App = () => (
+  <>
+    <Header />
+    <Body />
+    <Footer />
+  </>
+);
+
+export { App };
