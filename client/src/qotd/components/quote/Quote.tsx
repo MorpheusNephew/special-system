f2162f9b14f1e2bc96963c2f7e55923aff8ac02c
Jelani Jackson <jeljack86@gmail.com>
2020-06-07 09:40:51 -0500

    Started to add styling to Quote

--- client/src/qotd/components/quote/Quote.tsx
+++ client/src/qotd/components/quote/Quote.tsx
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from "react";
+import Paper from "@material-ui/core/Paper";
 import { getQuote } from "../../resources";
 import { QuoteResponse } from "../../models/quote";
 import { QuoteInfo } from "./quoteInfo/QuoteInfo";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    marginTop: "5%",
+  },
+});
 
 const Quote = () => {
   const [quoteResponse, setQuoteResponse] = useState<QuoteResponse | undefined>(
@@ -14,8 +22,10 @@ const Quote = () => {
     });
   }, []);
 
+  const styles = useStyles();
+
   return (
-    <>
+    <Paper className={styles.root}>
       {quoteResponse && (
         <QuoteInfo
           author={quoteResponse.author}
@@ -25,7 +35,7 @@ const Quote = () => {
           tags={quoteResponse.tags}
         />
       )}
-    </>
+    </Paper>
   );
 };
 
