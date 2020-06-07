f6671838e9d41c4c48190fa72645d80a4be86c0e
Jelani Jackson <jeljack86@gmail.com>
2020-06-07 09:40:29 -0500

    Added AppBar and Typography

--- client/src/qotd/components/header/Header.tsx
+++ client/src/qotd/components/header/Header.tsx
@@ -1,5 +1,13 @@
 import * as React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
 
-const Header = () => <header>Welcome to Quote of the Day</header>;
+const Header = () => (
+  <AppBar>
+    <Typography variant="h6" align="center">
+      Welcome to Quote of the Day app
+    </Typography>
+  </AppBar>
+);
 
 export { Header };
